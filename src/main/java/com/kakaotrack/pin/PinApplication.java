b651895e32c78a428270391976c19987c719aaef
Soojin6943 <sjhan0711@gmail.com>
2025-01-13 11:32:13 +0900

    init: 초기 환경 세팅 & health check

---
+++ src/main/java/com/kakaotrack/pin/PinApplication.java
@@ -0,0 +1,13 @@
+package com.kakaotrack.pin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing  // 엔티티 객체가 생성되거나 변경되었을때 자동으로 값 등록 가능
+@SpringBootApplication
+public class PinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PinApplication.class, args);
+    }
+}
