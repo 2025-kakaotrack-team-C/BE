4ab36967cfbd1bc7de3a3aa7bf110681c83bb9f6
Sangkyounge <shin8877123@naver.com>
2025-01-16 15:51:00 +0900

    Language, UserDepartment DB 생성 완료

---
+++ src/main/java/com/kakaotrack/pin/jwt/member/UserDepartment.java
@@ -0,0 +1,20 @@
+package com.kakaotrack.pin.jwt.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserDepartment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userDepartmentId;  // PK
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")  // FK
+    private Member member;
+
+    private int department;
+}
