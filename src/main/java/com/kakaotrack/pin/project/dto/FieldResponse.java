dab889333269814c847291bd4c266149d9e4d7b5
Soojin6943 <sjhan0711@gmail.com>
2025-01-20 16:26:18 +0900

    프로젝트 수정 오류 해결

--- src/main/java/com/kakaotrack/pin/project/dto/FieldResponse.java
+++ src/main/java/com/kakaotrack/pin/project/dto/FieldResponse.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class FieldResponse {
+    private final long fieldId;
     private final int department;
     private final int range;
 
-    public FieldResponse(int department, int range) {
+    public FieldResponse(long fieldId, int department, int range) {
+        this.fieldId = fieldId;
         this.department = department;
         this.range = range;
     }
