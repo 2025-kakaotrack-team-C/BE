39f4234fd0a33259d2bfccde4acbbad2df714bd7
sangkyounge <shin8877123@naver.com>
2025-01-21 11:01:17 +0900

    entity about review

---
+++ src/main/java/com/kakaotrack/pin/review/entity/Project_Member.java
@@ -0,0 +1,26 @@
+package com.kakaotrack.pin.review.entity;
+
+import com.kakaotrack.pin.domain.Project;
+import com.kakaotrack.pin.jwt.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project_Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long member_id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member user;
+
+    private String department;
+}
