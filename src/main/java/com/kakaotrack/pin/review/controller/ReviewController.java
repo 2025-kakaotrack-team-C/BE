5e949783c40957d103f8f17818ddf7b6c02d821d
sangkyounge <shin8877123@naver.com>
2025-01-21 11:56:49 +0900

    modify
    # Conflicts:
    #	src/main/java/com/kakaotrack/pin/review/controller/ReviewController.java
    #	src/main/java/com/kakaotrack/pin/review/dto/ReviewRequestDto.java
    #	src/main/java/com/kakaotrack/pin/review/repository/ReviewRepository.java
    #	src/main/java/com/kakaotrack/pin/review/service/ReviewService.java

--- src/main/java/com/kakaotrack/pin/review/controller/ReviewController.java
+++ src/main/java/com/kakaotrack/pin/review/controller/ReviewController.java
@@ -16,21 +16,21 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    // 1. 리뷰 작성
+    // 1. 리뷰 생성 (팀원 평가, 1-5점)
     @PostMapping
     public ResponseEntity<ReviewResponseDto> createReview(@RequestBody ReviewRequestDto requestDto) {
-        return ResponseEntity.ok(reviewService.createReview(requestDto, requestDto.getReviewerId()));
+        return ResponseEntity.ok(reviewService.createReview(requestDto));
     }
 
-    // 2. 내가 받은 모든 리뷰 조회
+    // 2. 내가 받은 모든 리뷰 조회 (누적 평점용)
     @GetMapping("/received")
-    public ResponseEntity<List<ReviewResponseDto>> getMyReceivedReviews(@RequestParam Long userId) {
-        return ResponseEntity.ok(reviewService.getMyReceivedReviews(userId));
+    public ResponseEntity<List<ReviewResponseDto>> getMyReceivedReviews() {
+        return ResponseEntity.ok(reviewService.getMyReceivedReviews());
     }
 
     // 3. 프로젝트별 받은 리뷰 조회
     @GetMapping("/projects/{projectId}/received")
-    public ResponseEntity<List<ReviewResponseDto>> getProjectReviews(@PathVariable Long projectId, @RequestParam Long userId) {
-        return ResponseEntity.ok(reviewService.getProjectReviews(projectId, userId));
+    public ResponseEntity<List<ReviewResponseDto>> getProjectReviews(@PathVariable Long projectId) {
+        return ResponseEntity.ok(reviewService.getProjectReviews(projectId));
     }
 }
