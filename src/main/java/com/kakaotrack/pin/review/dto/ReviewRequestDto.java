42445fde0be3df704d7b18382275a9a254630379
sangkyounge <shin8877123@naver.com>
2025-01-22 09:23:51 +0900

    ãmodify review

--- src/main/java/com/kakaotrack/pin/review/dto/ReviewRequestDto.java
+++ src/main/java/com/kakaotrack/pin/review/dto/ReviewRequestDto.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ReviewRequestDto {
     private Long projectId;
+    private Long reviewerId;  // 평가하는 사람 ID 추가
     private Long revieweeId;  // 평가 대상자 ID
     private Integer rating;   // 1~5 점수
 
     @Builder
     public ReviewRequestDto(Long projectId, Long revieweeId, Integer rating) {
         this.projectId = projectId;
+        this.reviewerId = revieweeId; // modify
         this.revieweeId = revieweeId;
         this.rating = rating;
     }
